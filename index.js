e0f7ffd758558685b40fa47529af60c96410443e
Yasirah <yasirahboyce1@gmail.com>
2021-11-08 15:26:49 +0000

    cc-13

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-//write your code here
+function averageArray(grades) {
+    let average = 0;
+  
+    for (let i = 0; i < grades.length; i++){ 
+    
+    average += grades[i]
+    }
+
+    average = average/grades.length
+  
+  return average
+}
+
+console.log(averageArray([25,26,27,28,29,30]))
